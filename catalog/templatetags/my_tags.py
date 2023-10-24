59ee0d0f942c6f567b515af5085bfd897037d28b
AlexandraAvtaykina <justalexandra@mail.ru>
2023-10-24 21:06:02 +0300

    homework 20.2 (проблема с картинками)

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,10 +1,18 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
-@register.filter()
-def media(val):
-    if val:
-        return f'/media/{val}'
-    return ''
+@register.filter
+def media_url(value):
+    """
+    Фильтр для преобразования пути в полный путь для доступа к медиафайлу.
+    """
+    media_root = settings.MEDIA_URL
+    return f"{media_root}{value}"
+
+
+@register.simple_tag()
+def mediafile(value):
+    return f'/media/{value}'
